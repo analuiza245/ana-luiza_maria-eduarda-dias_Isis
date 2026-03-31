3a22cff739555b4e614fbf174979a1e633de1f30
Maria Eduarda <dudadias.staa010425@gmail.com>
2026-03-31 09:54:19 -0300

    banco de dados

--- Trabalho.py
+++ Trabalho.py
@@ -1,8 +1,9 @@
 import mysql.connector
 
 banco = mysql.connector.connect(
-    host="10.30.29.183",
+    host="10.30.29.100",
     port=3309,
     user="root",
     password="root123"
+    database="Maria Eduarda Dias"
 )
