5e9fa76a10b76d7e780c1aead9e376f17264db00
Ana_luiza <luizasilvateixeira@gmail.com>
2026-03-31 11:33:40 -0300

    ultima

--- atividade.py
+++ atividade.py
@@ -36,3 +36,12 @@ valores = (user_id,)
 cursor.execute(sql, valores)
 banco.commit()
 
+email = input("Digite seu email")
+senha = input("Digite sua senha")
+cursor.execute(f"SELECT senha FROM USUARIO WHERE email = '{email}'")
+dado = cursor.fetchone()
+if dado:
+    if senha == dado[0]
+        print("acesso concedido")
+    else:
+        print("acesso negado")
