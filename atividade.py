2d8aad8e1e8af862e085c46399f16c0fbc7d6a68
ana <ana.clara13766@gmail.com>
2026-03-31 10:24:40 -0300

    mensagem

--- atividade.py
+++ atividade.py
@@ -11,7 +11,9 @@ banco = mysql.connector.connect(
 cursor = banco.cursor()
 
 cursor.execute("CREATE DATABASE IF NOT EXISTS analuiza_Isis_mariadias")
-cursor.execute("CREATE TABLE USUARIO (id INT, username VARCHAR(255), email VARCHAR(50), senha VARCHAR(100), idade INT)")
+
+#cursor.execute("CREATE TABLE USUARIO (id INT, username VARCHAR(255), email VARCHAR(50), senha VARCHAR(100), idade INT)")
+
 id_INT = int(input("id"))
 username = input("username")
 email = input("email")
@@ -27,4 +29,10 @@ dados = cursor.fetcha11()
 for linha in dados
     print(linha)
     
-    
+user_id = int(input("Digite seu usuario"))
+sql = "DELETE FROM USUARIO WHERE id = %s"
+valores = (user_id,)
+
+cursor.execute(sql, valores)
+banco.commit()
+
